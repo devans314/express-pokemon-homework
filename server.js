47a0951eee14a3a2cdf4d42a9e30ebc237ed45b7
Donald Evans <donaldevans@Donalds-MacBook-Pro.local>
2018-12-05 12:27:24 -0700

    JSON is visible in the browser

--- server.js
+++ server.js
@@ -1,5 +1,26 @@
 const express = require('express');
 const app = express();
+const Pokemon = require('./models/pokemon.js')
+const methodOverride = require('method-override')
+const bodyParser = require("body-parser");
+
+
+app.use(express.static('./public'))
+app.use(methodOverride('_method'));
+app.use(bodyParser.urlencoded({extended: false}));
+
+
+app.get("/pokemon", (req, res) => {
+    res.send(Pokemon);
+})
+
+
+
+
+
+
+
+
 
 
 app.listen(3000, ()=>{
